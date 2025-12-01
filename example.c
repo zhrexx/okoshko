cdb719eb009924f37b3a083cec20b741483dee1c
dev <jmoritz38@gmail.com>
2025-12-01 16:49:33 +0100

    new button module

--- example.c
+++ example.c
@@ -2,23 +2,41 @@
 
 #include "okoshko.h"
 #include "assets/font.h"
+#define OKO_BUTTON_IMPLEMENTATION
+#include "modules/button.h"
 
 #include <stdlib.h>
+
+const i32 WIDTH = 800;
+const i32 HEIGHT = 600;
 
 int main() {
     srand(time(NULL));
 
     oko_Font *f = oko_font_from_8x8(font8x8_basic);
     oko_init();
-    oko_Window *win = oko_create("Example", 800, 600);
-    oko_set_fps(win, 10000);
+    oko_Window *win = oko_create("Example", WIDTH, HEIGHT);
+    oko_set_fps(win, 100000);
 
+    oko_Button btn1 = oko_button_create(300, 250, 200, 50, "Click Me!", f);
+
+    u64 i = 0;
     while (oko_is_running(win))
     {
+
+        if (oko_button_update(win, &btn1))
+        {
+            log_info("Button was pressed");
+        }
+
         oko_begin_drawing(win);
+
         oko_clear(win, 0x00);
+
+        oko_button_draw(win, &btn1);
+
+        oko_draw_text(win, oko_format("FPS: %llu", oko_get_fps(win)), f, 20, 20, 1.5f, 0xFFFFFFFF);
         oko_end_drawing(win);
-        log_info(oko_format("FPS: %0u", oko_get_fps(win)));
     }
 
     oko_destroy(win);
---
+++ modules/button.h
@@ -0,0 +1,103 @@
+// TI = Text Input
+#pragma once
+
+// TODO: implement special chars and , .
+
+#include <ctype.h>
+#include <string.h>
+
+#include "../okoshko.h"
+
+
+typedef enum {
+    OKO_BUTTON_IDLE,
+    OKO_BUTTON_HOVER,
+    OKO_BUTTON_PRESSED
+} oko_ButtonState;
+
+typedef struct {
+    oko_Rect bounds;
+    const char *text;
+    oko_Font *font;
+    float text_scale;
+
+    u32 color_idle;
+    u32 color_hover;
+    u32 color_pressed;
+    u32 text_color;
+
+    oko_ButtonState state;
+    u8 was_pressed;
+} oko_Button;
+
+static inline oko_Button oko_button_create(i32 x, i32 y, i32 w, i32 h, const char *text, oko_Font *font);
+static inline u8 oko_button_update(oko_Window *win, oko_Button *btn);
+
+static void oko_button_draw(oko_Window *win, oko_Button *btn);
+
+#ifdef OKO_BUTTON_IMPLEMENTATION
+static inline oko_Button oko_button_create(i32 x, i32 y, i32 w, i32 h, const char *text, oko_Font *font) {
+    oko_Button btn = {0};
+    btn.bounds = (oko_Rect){x, y, w, h};
+    btn.text = text;
+    btn.font = font;
+    btn.text_scale = 1.0f;
+
+    btn.color_idle = 0xFF404040;
+    btn.color_hover = 0xFF606060;
+    btn.color_pressed = 0xFF202020;
+    btn.text_color = 0xFFFFFFFF;
+
+    btn.state = OKO_BUTTON_IDLE;
+    btn.was_pressed = 0;
+
+    return btn;
+}
+
+static inline u8 oko_button_update(oko_Window *win, oko_Button *btn) {
+    i32 mx = win->mouse.x;
+    i32 my = win->mouse.y;
+
+    u8 inside = (mx >= btn->bounds.x && mx < btn->bounds.x + btn->bounds.w &&
+                 my >= btn->bounds.y && my < btn->bounds.y + btn->bounds.h);
+
+    u8 clicked = 0;
+
+    if (inside) {
+        if (win->mouse.left) {
+            btn->state = OKO_BUTTON_PRESSED;
+            btn->was_pressed = 1;
+        } else {
+            if (btn->was_pressed) {
+                clicked = 1;
+            }
+            btn->state = OKO_BUTTON_HOVER;
+            btn->was_pressed = 0;
+        }
+    } else {
+        btn->state = OKO_BUTTON_IDLE;
+        btn->was_pressed = 0;
+    }
+
+    return clicked;
+}
+
+static void oko_button_draw(oko_Window *win, oko_Button *btn) {
+    u32 color;
+    switch (btn->state) {
+    case OKO_BUTTON_HOVER:   color = btn->color_hover; break;
+    case OKO_BUTTON_PRESSED: color = btn->color_pressed; break;
+    default:                 color = btn->color_idle; break;
+    }
+
+    oko_fill_rect(win, btn->bounds, color);
+
+    oko_draw_rect(win, btn->bounds, 0xFFAAAAAA);
+
+    if (btn->text && btn->font) {
+        i32 text_x = btn->bounds.x + 10;
+        i32 text_y = btn->bounds.y + (btn->bounds.h - btn->font->size) / 2;
+        oko_draw_text(win, btn->text, btn->font, text_x, text_y, btn->text_scale, btn->text_color);
+    }
+}
+#endif
